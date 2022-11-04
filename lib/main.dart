3ba06b76aea1de40934eefb0d685bced1de1ddb7
sochoall <sochoa@libelulasoft.com>
2022-11-04 18:40:28 -0500

    REFACTOR: Code splitted into small files to make it readable

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
+import './widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
-import './transaction.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'new shoes', amount: 12.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 15.53,
-        date: DateTime.now()),
-  ];
   MyApp({super.key});
+
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,8 +21,8 @@ class MyApp extends StatelessWidget {
           body: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
+            children: const <Widget>[
+              SizedBox(
                 width: double.infinity,
                 child: Card(
                   color: Colors.amber,
@@ -36,69 +30,7 @@ class MyApp extends StatelessWidget {
                   child: Text("CHART!"),
                 ),
               ),
-              Card(
-                elevation: 5,
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: const <Widget>[
-                      TextField(
-                        decoration: InputDecoration(labelText: "Title"),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: "Amount"),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Column(
-                children: transactions.map((e) {
-                  return Card(
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            "\$${e.amount}",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              e.title,
-                              textAlign: TextAlign.left,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                              ),
-                            ),
-                            Text(
-                              // DateFormat('yyyy-MM-dd').format(e.date),
-                              DateFormat.yMMMd().format(e.date),
-                              style: const TextStyle(color: Colors.grey),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-              )
+              UserTransactions()
             ],
           )),
     );
