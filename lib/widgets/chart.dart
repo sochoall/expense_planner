a2ea4f9dcb50d74ab0e63c3fdfaaad427043596d
sochoall <sochoa@libelulasoft.com>
2022-11-05 12:52:36 -0500

    ADD: Chart file and transaction list to iterate

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,41 @@
+import '../models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MyApp extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  const MyApp(this.recentTransactions, {super.key});
+
+  List<Map<String, Object>> get grupedTransactionsValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (int i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[
+          Column(),
+        ],
+      ),
+    );
+  }
+}
