3d4d92f805847b420ed7be4921aaef33fb33d080
sochoall <sochoa@libelulasoft.com>
2022-11-07 11:56:30 -0500

    REMOVE: SizedBox now we're dynamically calculating the size

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,103 +10,99 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 450,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  "No transaction added yet!",
-                  style: Theme.of(context).textTheme.headline1,
+    return transactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text(
+                "No transaction added yet!",
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              SizedBox(
+                height: 200,
+                child: Image.asset(
+                  "assets/images/waiting.png",
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(
-                  height: 60,
+              )
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text("\$${transactions[index].amount}"))),
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                  ),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  trailing: IconButton(
+                    onPressed: () => deleteTx(transactions[index].id),
+                    icon: const Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                  ),
                 ),
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    "assets/images/waiting.png",
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                          padding: const EdgeInsets.all(6),
-                          child: FittedBox(
-                              child: Text("\$${transactions[index].amount}"))),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                    ),
-                    subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: IconButton(
-                      onPressed: () => deleteTx(transactions[index].id),
-                      icon: const Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                    ),
-                  ),
-                );
-                // return Card(
-                //   child: Row(
-                //     children: <Widget>[
-                //       Container(
-                //         margin: const EdgeInsets.symmetric(
-                //             vertical: 10, horizontal: 15),
-                //         decoration: BoxDecoration(
-                //           border: Border.all(
-                //             color: Theme.of(context).primaryColor,
-                //             width: 2,
-                //           ),
-                //         ),
-                //         padding: const EdgeInsets.all(10),
-                //         child: Text(
-                //           "\$${transactions[index].amount.toStringAsFixed(2)}",
-                //           style: TextStyle(
-                //             fontFamily: 'Open Sans',
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 20,
-                //             color: Theme.of(context).primaryColor,
-                //           ),
-                //         ),
-                //       ),
-                //       Column(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: <Widget>[
-                //           Text(
-                //             transactions[index].title,
-                //             textAlign: TextAlign.left,
-                //             style: const TextStyle(
-                //               fontFamily: 'Open Sans',
-                //               fontWeight: FontWeight.bold,
-                //               fontSize: 16,
-                //             ),
-                //           ),
-                //           Text(
-                //             // DateFormat('yyyy-MM-dd').format(e.date),
-                //             DateFormat.yMMMd().format(transactions[index].date),
-                //             style: const TextStyle(
-                //               fontFamily: 'Open Sans',
-                //               color: Colors.grey,
-                //             ),
-                //           )
-                //         ],
-                //       ),
-                //     ],
-                //   ),
-                // );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+              );
+              // return Card(
+              //   child: Row(
+              //     children: <Widget>[
+              //       Container(
+              //         margin: const EdgeInsets.symmetric(
+              //             vertical: 10, horizontal: 15),
+              //         decoration: BoxDecoration(
+              //           border: Border.all(
+              //             color: Theme.of(context).primaryColor,
+              //             width: 2,
+              //           ),
+              //         ),
+              //         padding: const EdgeInsets.all(10),
+              //         child: Text(
+              //           "\$${transactions[index].amount.toStringAsFixed(2)}",
+              //           style: TextStyle(
+              //             fontFamily: 'Open Sans',
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 20,
+              //             color: Theme.of(context).primaryColor,
+              //           ),
+              //         ),
+              //       ),
+              //       Column(
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //         children: <Widget>[
+              //           Text(
+              //             transactions[index].title,
+              //             textAlign: TextAlign.left,
+              //             style: const TextStyle(
+              //               fontFamily: 'Open Sans',
+              //               fontWeight: FontWeight.bold,
+              //               fontSize: 16,
+              //             ),
+              //           ),
+              //           Text(
+              //             // DateFormat('yyyy-MM-dd').format(e.date),
+              //             DateFormat.yMMMd().format(transactions[index].date),
+              //             style: const TextStyle(
+              //               fontFamily: 'Open Sans',
+              //               color: Colors.grey,
+              //             ),
+              //           )
+              //         ],
+              //       ),
+              //     ],
+              //   ),
+              // );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
