6d751c6664242946149841f8e97a8da9c7608a47
sochoall <sochoa@libelulasoft.com>
2022-11-08 09:11:44 -0500

    REFACTOR: Column wrapped with layoutBuilder to make it easier to adapt to different sizes

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,28 +11,34 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: <Widget>[
-              Text(
-                "No transaction added yet!",
-                style: Theme.of(context).textTheme.headline1,
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  "assets/images/waiting.png",
-                  fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  "No transaction added yet!",
+                  style: Theme.of(context).textTheme.headline1,
                 ),
-              )
-            ],
-          )
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.4,
+                  child: Image.asset(
+                    "assets/images/waiting.png",
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (context, index) {
               return Card(
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
                 child: ListTile(
                   leading: CircleAvatar(
                     radius: 30,
@@ -43,14 +49,24 @@ class TransactionList extends StatelessWidget {
                   ),
                   title: Text(
                     transactions[index].title,
+                    style: Theme.of(context).textTheme.headline1,
                   ),
                   subtitle:
                       Text(DateFormat.yMMMd().format(transactions[index].date)),
-                  trailing: IconButton(
-                    onPressed: () => deleteTx(transactions[index].id),
-                    icon: const Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? TextButton.icon(
+                          onPressed: () => deleteTx(transactions[index].id),
+                          icon: const Icon(Icons.delete),
+                          label: const Text("Delete"),
+                          style: ButtonStyle(
+                            foregroundColor: MaterialStateProperty.all<Color>(
+                                Theme.of(context).errorColor),
+                          ))
+                      : IconButton(
+                          onPressed: () => deleteTx(transactions[index].id),
+                          icon: const Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                        ),
                 ),
               );
               // return Card(
